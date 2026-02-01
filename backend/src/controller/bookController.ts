3208e44370ca62639a34cd5d97211dcf0a6899fb
Sahil Gupta <sahilhsgupta7@gmail.com>
2026-02-02 00:17:20 +0530

    feat: added endpoint for GET /books/:id

--- backend/src/controller/bookController.ts
+++ backend/src/controller/bookController.ts
@@ -22,3 +22,32 @@ export const fetchBooksHandler = async (
     const books = await getBooksByUserId(req.user?.id || "", prisma);
     return res.status(200).json(books);
 }
+
+
+// GET /books/:id
+export const fetchBookHandler = async (
+    req: Request<{ userId: string, id: string }, unknown, unknown>,
+    res: Response<BookResponse>,
+    next: NextFunction
+) => {
+    const book = await getBookById(req.params.id, req.user?.id || "", prisma);
+    return res.status(200).json(book);
+}
+
+// export const updateBookHandler = async (
+//     req: Request<{ userId: string }, unknown, BookUpdateRequest>,
+//     res: Response<BookResponse>,
+//     next: NextFunction
+// ) => {
+//     const book = await updateBook(req.params.id, req.body, prisma);
+//     return res.status(200).json(book);
+// }
+
+// export const deleteBookHandler = async (
+//     req: Request<{ userId: string }, unknown, unknown>,
+//     res: Response<BookResponse>,
+//     next: NextFunction
+// ) => {
+//     const book = await deleteBook(req.params.id, prisma);
+//     return res.status(200).json(book);
+// }
